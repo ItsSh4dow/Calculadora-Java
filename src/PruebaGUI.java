10793812d613871408918eb94e88da12b14cf3a7
osvi <joseconstantino987@gmail.com>
2022-09-21 11:20:08 -0500

    Calculator V1

--- src/PruebaGUI.java
+++ src/PruebaGUI.java
@@ -2,8 +2,6 @@ import javax.swing.JFrame;
 
 public class PruebaGUI {
     public static void main(String[] args){
-        //la Clase se refiere al archivo q se pretende probar
-
         Calculadora prueba = new Calculadora();
         prueba.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         prueba.setSize(248,200);
